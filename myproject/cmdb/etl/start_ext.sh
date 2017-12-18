01357952dd1659308a599e764652c01356eaaef3
dennis <dennis.zhang@cargosmart.com>
2017-12-18 16:28:12 +0800

    fix start_ext.sh path issue

--- myproject/cmdb/etl/start_ext.sh
+++ myproject/cmdb/etl/start_ext.sh
@@ -1,4 +1,4 @@
-WORKDIR=`pwd`
+WORKDIR=/home/itop/scripts/mycmdb/myproject/cmdb/etl
 python3 $WORKDIR/ext_vc.py --host vc06
 python3 $WORKDIR/ext_vc.py --host vc02
 python3 $WORKDIR/ext_vc.py --host ppvc06
