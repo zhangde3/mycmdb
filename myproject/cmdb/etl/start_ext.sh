8a8432bff887a509250ccf5e6c4be7aabbcbc665
dennis <dennis.zhang@cargosmart.com>
2017-12-18 15:15:10 +0800

    fix start_ext.sh path issue

--- myproject/cmdb/etl/start_ext.sh
+++ myproject/cmdb/etl/start_ext.sh
@@ -1,3 +1,3 @@
-python3 ext_vc.py --host vc06
-python3 ext_vc.py --host vc02
-python3 ext_vc.py --host ppvc06
+python3 .ext_vc.py --host vc06
+python3 .ext_vc.py --host vc02
+python3 .ext_vc.py --host ppvc06
